1e221a4145727a4f37c724e6fba9f9b89b69a795
Daniella Bojado <daniella.bojado@gmail.com>
2020-09-22 16:03:26 -0500

    add acquire python file

--- acquire.py
+++ acquire.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 from env import host, username, password
 
-def get_connection(db, user=username, host=host, password=password):
+def get_connection(db, username=username, host=host, password=password):
     return f'mysql+pymysql://{username}:{password}@{host}/{db}'
 
 def get_telco_data():
