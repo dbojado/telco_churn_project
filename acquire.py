11ddd66627995a312fa095b7328633e73170212b
Daniella Bojado <daniella.bojado@gmail.com>
2020-09-22 15:41:17 -0500

    add acquire python file

--- acquire.py
+++ acquire.py
@@ -1,27 +1,23 @@
-import os
 import pandas as pd
 import numpy as np
-import env
+import os
+from env import host, username, password
 
 def get_connection(db, username=env.username, host=env.host, password=env.password):
     return f'mysql+pymysql://{username}:{password}@{host}/{db}'
 
-    
-def get_telecom_data():
-    file = 'telecom_data.csv'
+def get_telco_data():
+    file = 'telco_data.csv'
 
     if os.path.isfile(file):
-        return pd.read_csv('telecom_data.csv')
+        return pd.read_csv('telco_data.csv')
     else:
         df = pd.read_sql("""
-                        select *
-                        from customers
-                        join internet_service_types using(internet_service_type_id)
-                        join contract_types using(contract_type_id)
-                        join payment_types using(payment_type_id);
+                        SELECT *
+                        FROM customers;
                         """,
                         get_connection('telco_churn')
                         )
         
-        df.to_csv('telecom_data.csv', index=False)
+        df.to_csv('telco_data.csv', index=False)
         return df
