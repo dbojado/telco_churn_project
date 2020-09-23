adb72e600e5dc722a24883bd4b89ed4970a22ba2
Daniella Bojado <daniella.bojado@gmail.com>
2020-09-23 06:39:58 -0500

    add prepare python file

--- prepare.py
+++ prepare.py
@@ -19,18 +19,9 @@ def telco_data_prep():
 
 
 #Function for spliting data (train, test, validate)
-def train_test_validate(telco_data_df):
-    from sklearn.model_selection import train_test_split
+def train_test_validate():
 
-    train_validate, test = train_test_split(telco_data_df, test_size = .10, random_state = 123)
-
-    train, validate = train_test_split(train_validate, test_size = .20, random_state = 123)
-
-    print("train shape: ", train.shape, ", validate shape: ", validate.shape, ", test shape: ", test.shape)
-
-    total = telco_data_df.count()[0]
-    print("\ntrain percent: ", round(((train.shape[0])/total),2) * 100, 
-            ", validate percent: ", round(((validate.shape[0])/total),2) * 100, 
-            ", test percent: ", round(((test.shape[0])/total),2) * 100)
+    train_validate, test = train_test_split(df, test_size = .2, random_state = 123)
+    train, validate = train_test_split(train_validate, test_size = .3, random_state = 123)
 
     return train, validate, test
